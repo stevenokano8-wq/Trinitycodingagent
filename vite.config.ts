27c57615faf111bfc675cec0f61244c633b7e050
stevenokano8-wq <stevenokano8@gmail.com>
2026-07-26 18:52:11 +0100

    Synchronized project with Sovereign Agent build cluster

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({ plugins: [react()], server: { port: 3000 } });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+  },
+});

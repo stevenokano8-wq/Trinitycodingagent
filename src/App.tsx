670ecee9f869280cb7d72000638229b433a0b16b
stevenokano8-wq <stevenokano8@gmail.com>
2026-07-27 11:55:49 +0100

    Synchronized project with Sovereign Agent build cluster

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@
 import React from 'react';
 
 function App() {
-  return <div className="h-screen w-screen"></div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center text-3xl text-white">
+      The UI is now visible
+    </div>
+  );
 }
 
 export default App;
